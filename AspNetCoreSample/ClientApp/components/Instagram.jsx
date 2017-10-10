1bf120bd7347d13b6d6a584866df0b15c70a9191
DaniilSokolyuk <danoov@gmail.com>
2017-10-10 22:51:49 +0300

    debounce added

--- AspNetCoreSample/ClientApp/components/Instagram.jsx
+++ AspNetCoreSample/ClientApp/components/Instagram.jsx
@@ -2,19 +2,41 @@
 import User from './User'
 import { PhotoSwipe } from 'react-photoswipe';
 
+ function debounce(callback, delay, context) {
+    let timeout = null;
+    return function() {
+      if (timeout)
+        clearTimeout(timeout);
+
+     let args = arguments;
+      timeout = setTimeout(() => {
+        callback.apply(context || null, args);
+        timeout = null;
+      }, delay | 0);
+    };
+  }
+
 export default class Instagram extends Component {
     state = {
         users: this.props.search.users,
+        searchText: this.props.search.text,
         loading: false
     };
 
-    handleChange = (evt) => {
+    componentWillUpdate(prevProps, prevState) {
+        const { searchText } = this.state;
+        if (prevState.searchText !== searchText && searchText) {
+            this.request(searchText);
+        }
+    }
+
+    request = debounce(searchText =>
+    {
         this.setState({
             loading: true
         });
-
         var xhr = new XMLHttpRequest();
-        xhr.open('GET', "/Home/Search/?text=" + evt.target.value, true);
+        xhr.open('GET', "/Home/Search/?text=" + searchText, true);
         xhr.setRequestHeader("Content-Type", "application/json");
         xhr.onload = () => {
             var data = JSON.parse(xhr.responseText);
@@ -24,24 +46,29 @@ export default class Instagram extends Component {
             });
         };
         xhr.send();
-        evt.preventDefault();
+    }, 200)
+
+    handleChange = (evt) => {
+        this.setState({
+            searchText: evt.target.value,
+        });
     };
 
     render() {
 
         var userNodes = this.state.users.map(user =>
-            <User user={user.user} key={user.user.pk}>{user.position}</User>
+            <User user={user.user} key={user.user.pk}></User>
         );
 
         return (
             <div>
                 <input
                     type="text"
-                    value={this.state.value}
+                    value={this.state.searchText}
                     onChange={this.handleChange}
                 />
 
-                <h1>Comments</h1>
+                <h1>Users</h1>
                 <ol className="userList">
                     {userNodes}
                 </ol>
--- AspNetCoreSample/ClientApp/components/User.jsx
+++ AspNetCoreSample/ClientApp/components/User.jsx
@@ -7,7 +7,9 @@ export default class User extends Component {
         return (
             <li>
                 <Avatar picUrl={this.props.user.profile_pic_url} name={this.props.user.full_name} />
-
+                <p>{this.props.user.username}</p>
+                <p>{this.props.user.byline}</p>
+                {this.props.user.is_private && <h3> Private </h3>}
                 <strong>{this.props.user.full_name}</strong>
                 {this.props.children}
             </li>
